de1d2c3d0d489587614bd21d9eba3ac482870214
= <chanmichou37@gmail.com>
2025-05-16 20:38:05 -0400

    Ajouts dans le pied de page et mise à jour du css

--- footer.php
+++ footer.php
@@ -1,6 +1,39 @@
 <footer class="piedpage">
+  <div class="piedpage__grille">
+      <div class="piedpage__grille-carte">          
+        <h5>Menu</h5>
+          <article class="piedpage__carte">    
+            <ul >
+              <li><img src="https://s2.svgbox.net/hero-outline.svg?ic=home&color=000" width="28" height="28"><a href="#">accueil</a></li>
+              <li><img src="https://s2.svgbox.net/materialui.svg?ic=mark_chat_unread&color=000" width="28" height="28"><a href="#"> Blogue</a></li>
+              <li><img src="https://s2.svgbox.net/hero-outline.svg?ic=document&color=000" width="28" height="28"><a href="#">À propos</a></li>
+            </ul>
+          </article>
+      </div>
+      <div class="piedpage__grille-carte">
+        <h5>Notre infolettre</h5>
+        <article class="piedpage__carte">      
+        <form action="" class="recherche">
+          <input type="search">
+          <input type="button">
+        </form>
+      </article>
+    </div>
+    <div class="piedpage__grille-carte">
+        <h5>Galeries de photos</h5>
+        <article  class="piedpage__carte">              
+            <div class="piedpage__carrousel">
+                <img src="https://s2.svgbox.net/materialui.svg?ic=arrow_left&color=000" width="35" height="35" alt="icone flèche gauche">
+                <img class="piedpage__carrousel-petit" src="http://localhost/Voyage-33W-ete-25/wp-content/uploads/2025/05/Voilier-mer-bleue.jpg" alt="Alaska">
+                <img src="https://s2.svgbox.net/materialui.svg?ic=arrow_right&color=000" width="35" height="35" alt="icon flèche droite">
+            </div>        
+        </article>
+    </div>    
+  </div>
+  <div class="piedpage__credits">
     <p>Tous droits réservés : Je voyage ☀⛱ - 2025</p>
     <p>Création : Chantal Pépin</p>
+    </div>
 </footer>
 <?php  wp_footer(); ?>
 </body>
