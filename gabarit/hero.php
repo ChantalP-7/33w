536bbd83ea9f370c0894bf41c7c9318a4f7034bf
= <chanmichou37@gmail.com>
2025-06-27 10:42:17 -0400

    Ajout de panneaux et de champs dans mon customizer

--- gabarit/hero.php
+++ gabarit/hero.php
@@ -4,19 +4,20 @@
  *  permet d'afficher la section hero
  * */ 
 ?>
-
 <?php
-
-
-
-$hero_auteur = get_theme_mod('hero_auteur', 'Default Title');
+$hero_auteur = get_theme_mod('hero_auteur', 'Mireille Fouchon');
+$hero_adresse = get_theme_mod('hero_adresse', '18, rue des Grenouilles, Paris, Canada');
 
 ?>
 
+<div class="hero__contenu">
+<h1 class="hero__titre"><?php bloginfo('name') ?></h1>
+<p class="hero__description">
+    <?php bloginfo('description') ?></p>
 <p>Auteur du thème : <?= $hero_auteur?></p>
+<p>Adresse du Club : <?= $hero_adresse?></p>
 
-<div cla></div>
-
+</div>
 <?php 
 
 
