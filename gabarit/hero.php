4ed40bd8f824925bbcf5a0e1ebebd74da13784c6
= <chanmichou37@gmail.com>
2025-06-27 09:49:12 -0400

    Ajout d'un dossier functions et de fichiers configuration et mon-customizer

--- gabarit/hero.php
+++ gabarit/hero.php
@@ -4,3 +4,20 @@
  *  permet d'afficher la section hero
  * */ 
 ?>
+
+<?php
+
+
+
+$hero_auteur = get_theme_mod('hero_auteur', 'Default Title');
+
+?>
+
+<p>Auteur du thème : <?= $hero_auteur?></p>
+
+<div cla></div>
+
+<?php 
+
+
+?>
