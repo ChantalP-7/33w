9b4d18a4553ac06361a4a2404798f10d51d7da83
= <chanmichou37@gmail.com>
2025-08-14 14:13:08 -0400

    Ajout de l'accordeon pour le menu de categories. Il reste a faire le css et l'animation de cette partie

---
+++ page.php
@@ -0,0 +1,31 @@
+<?php
+
+/**
+ * le modèle single.php
+ * Représente le modèle par défaut
+ */
+
+?>
+
+<?php get_header() ?>
+<h3>Trace à retirer -------------------- page d'exemple trace ------------------------</h3>
+<section class="populaire">
+  <?php if (have_posts()) {
+    while (have_posts()) {
+      /* affiche l'image « mise en avant » miniature */
+      the_post();
+      the_post_thumbnail('large');
+  ?>
+      <h2 class="text-left"><?php
+          /* affiche le titre pricipal du « post » */
+          the_title(); ?></h2>
+
+  <?php
+      /* cette fontion permet d'afficher l'ensemble du contenu (même les images) du post (article ou page)*/
+      the_content();
+      edit_post_link();
+    }
+  } ?>
+</section>
+<?php get_footer();
+  
